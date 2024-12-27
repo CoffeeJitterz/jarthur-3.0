44adf6f26d89cd0455252ddfbc73e6bbaae6a84a
CoffeeJitterz <josh.millard10@gmail.com>
2024-12-26 19:05:21 -0500

    tiled background implimented

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="background">
-         <div className="page">
+    <div className="page" > 
             <Image 
             src="/jarthur_logo.png"
             width={650}
             height={600}
             alt="cartoon faces"
             />
-        </div>
     </div>
   );
 }
