cca576ae8416144105fa9b3a6cf93d8ea099be26
CoffeeJitterz <josh.millard10@gmail.com>
2024-12-29 10:06:48 -0500

    colour change button wealy implimented on milk page

--- src/app/milk/page.js
+++ src/app/milk/page.js
@@ -1,6 +1,22 @@
+"use client"
+import {useState, useEffect} from 'react'
+
 export default function About() {
+
+const [color, setColor] = useState("blue");
+
+const click = color => {
+    setColor(color)
+}
+
+useEffect(() => {
+    document.body.style.backgroundColor = color
+}, [color])
+
+
     return (
-      <div className="page">
+      <div className="app">
+        <button onClick={() => click("yellow")}>Click Me For Milk</button>
         <h1>Milk</h1>
       </div>
     );
