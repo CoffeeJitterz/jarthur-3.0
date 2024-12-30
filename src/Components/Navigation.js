45e6fc4be0c64b138387c0fc236edf7de10f44a1
CoffeeJitterz <josh.millard10@gmail.com>
2024-12-30 16:38:01 -0500

    card component added to about page

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,6 +2,7 @@
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import {useState, useEffect} from 'react';
+import Card from "./Card";
 
 export default function Navigation() {
     
@@ -24,33 +25,35 @@ export default function Navigation() {
     return (
         <div className="navigation">
 
-            <Link 
-            className="link" 
-            onClick={() => {click_pattern(pattern); click_color("Blue")}} 
-            href={{
-                pathname:"/",
-                query: {
-                    pattern: pattern
-                }}}>
-            Home</Link>
-            <Link 
-            className="link" onClick={() => {click_pattern(pattern); click_color("Yellow")}} 
-            href={{
-                pathname: "/about",
-                query: {
-                    pattern: pattern
-                }}}>
-            About</Link>
-            <Link 
-            className="link" 
-            onClick={() => {click_pattern(pattern); click_color("Brown")}} 
-            href={{pathname: "/milk",
-                    query: {
-                        pattern: pattern
-                    }
-            }}>
-            milk</Link>
-        </div>
+<Link 
+className="link" 
+onClick={() => {click_pattern(pattern); click_color("Blue")}} 
+href={{
+    pathname:"/",
+    query: {
+        pattern: pattern
+    }}}>
+Home</Link>
+<Link 
+className="link" onClick={() => {click_pattern(pattern); click_color("Yellow")}} 
+href={{
+    pathname: "/about",
+    query: {
+        pattern: pattern
+    }}}>
+About</Link>
+<Link 
+className="link" 
+onClick={() => {click_pattern(pattern); click_color("Brown")}} 
+href={{pathname: "/milk",
+        query: {
+            pattern: pattern
+        }
+}}>
+milk</Link>
+</div>
+
+
 
     );
   }
--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Card from "@/Components/Card";
 
 export default async function About({searchParams}) {
 
@@ -6,14 +7,18 @@ const params = await searchParams;
 const pattern = params.pattern; 
 
   return (
+
     <div className={`page_${pattern}`}>
-        <h1 className="about_header">About</h1>
+          <Card>
+          <h1 className="about_header">About</h1>
         <Image 
         src="/1.png"
         width={450}
         height={600}
         alt="wedding photo"
         />
+      </Card>
+
     </div>
   );
 }
