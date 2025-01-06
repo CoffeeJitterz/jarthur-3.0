99f9fbcddb6ebc3290387b5018a0d44488095fed
CoffeeJitterz <josh.millard10@gmail.com>
2025-01-05 21:09:29 -0500

    not sure what's going on

--- src/app/drawings/[id]/page.js
+++ src/app/drawings/[id]/page.js
@@ -4,14 +4,15 @@ function page({params}) {
 
 const drawings = [{id:"poop", title:"crazy caves", description: "that was a crazy part of the cave"},
                   {id:"pee", title:"wild caves", description: "that was a wild part of the cave"}];
-const drawingRender = drawings.map(drawing => {})
+const drawingRender = drawings.map(drawing => <h1>drawing</h1>)
 
   return (
     <div>
-      {params.id}
+      {/* {params.id}
       <h1>{drawings[1].id}</h1>
       <h1>{drawings[1].title}</h1>
-      <h3>{drawings[1].description}</h3>
+      <h3>{drawings[1].description}</h3> */}
+      {drawingRender}
     </div>
   )
 }
