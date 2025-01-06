77c5964944591fbf2743126f50917ef1b7e92ce6
CoffeeJitterz <josh.millard10@gmail.com>
2025-01-05 21:06:04 -0500

    more fiddling

--- src/app/drawings/[id]/page.js
+++ src/app/drawings/[id]/page.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 function page({params}) {
 
-const drawings = [{title:"crazy caves", description: "that was a crazy part of the cave"},
-                  {title:"wild caves", description: "that was a wild part of the cave"}];
+const drawings = [{id:"poop", title:"crazy caves", description: "that was a crazy part of the cave"},
+                  {id:"pee", title:"wild caves", description: "that was a wild part of the cave"}];
+const drawingRender = drawings.map(drawing => {})
 
   return (
     <div>
       {params.id}
+      <h1>{drawings[1].id}</h1>
       <h1>{drawings[1].title}</h1>
       <h3>{drawings[1].description}</h3>
     </div>
