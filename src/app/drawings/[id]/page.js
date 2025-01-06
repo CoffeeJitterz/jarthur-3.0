1c4915acdca4cdb4cba5097a2bd8c8ea49897545
CoffeeJitterz <josh.millard10@gmail.com>
2025-01-06 10:15:06 -0500

    got map() working for each page

--- src/app/drawings/[id]/page.js
+++ src/app/drawings/[id]/page.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 function page({params}) {
 
+
 const drawings = [{id:"poop", title:"crazy caves", description: "that was a crazy part of the cave"},
-                  {id:"pee", title:"wild caves", description: "that was a wild part of the cave"}];
-const drawingRender = drawings.map(drawing => <h1>{drawing.title}</h1>)
+                  {id:"pee", title:"wild caves", description: "that was a wild part of the cave"}]
+const drawingRender = drawings.map(drawing => {if(drawing.id === params.id) {return <div><h1>{drawing.title}</h1><h2>{drawing.description}</h2></div>}})
 
   return (
     <div>
