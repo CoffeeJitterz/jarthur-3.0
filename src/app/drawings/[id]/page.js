8e3ae96670dc7d160b5b6e9805d8950cfc2df5db
CoffeeJitterz <josh.millard10@gmail.com>
2025-01-05 20:55:00 -0500

    playing with dynamic pages, objects and arrays

--- src/app/drawings/[id]/page.js
+++ src/app/drawings/[id]/page.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
-function page({ params }) {
+function page({params}) {
+
+const drawings = [{title:"crazy caves", description: "that was a crazy part of the cave"},
+                  {title:"wild caves", description: "that was a wild part of the cave"}];
+
   return (
-    <div>{params.id}</div>
+    <div>
+      {params.id}
+      <h1>{drawings[1].title}</h1>
+      <h3>{drawings[1].description}</h3>
+    </div>
   )
 }
 
