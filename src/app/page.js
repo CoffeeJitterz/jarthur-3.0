c8ff7dc0cdc8fe0f7dbf8f1776e547b33dc82f6b
CoffeeJitterz <josh.millard10@gmail.com>
2025-01-06 16:47:00 -0500

    test page deleted

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,18 @@
 
 import Image from "next/image";
-import Card from "@/Components/Card"
-
 
 export default async function Home({searchParams}) {
-
   const params = await searchParams;
   const pattern = params.pattern;
   console.log(pattern);
   return (
     <div className={pattern ? `page_${pattern}` : 'page'} > 
-            <Card color={"Yellow"}>
             <Image 
             src="/jarthur_logo.png"
             width={650}
             height={600}
             alt="cartoon faces"
             />
-            </Card>
     </div>
 
   );
