54574a566071c129a423e1da71003aa0493ac3a9
CoffeeJitterz <josh.millard10@gmail.com>
2025-01-13 10:53:48 -0500

    drawings resized

--- src/Data/drawings.js
+++ src/Data/drawings.js
@@ -2,27 +2,27 @@ const drawings = [{
     id:"1", 
     title:"crazy caves", 
     description: "that was a crazy part of the cave", 
-    image: "/drawings/1.jpg"
+    image: "/drawings/1.png"
   }, {
     id:"2", 
     title:"wild caves", 
     description: "that was a wild part of the cave", 
-    image: "/drawings/2.jpg"
+    image: "/drawings/2.png"
   }, {
     id:"3",
     title:"wonderful caves",
     description: "that was a wonderful part of the cave",
-    image: "/drawings/3.jpg"
+    image: "/drawings/3.png"
   }, {
     id:"4",
     title:"cryptic caves",
     description: "that was a cryptic part of the cave",
-    image: "/drawings/4.jpg"
+    image: "/drawings/4.png"
   }, {
     id:"5",
     title:"scary caves",
     description: "that was a scary part of the cave",
-    image: "/drawings/5.jpg"
+    image: "/drawings/5.png"
   }]
 
   export default drawings
--- src/app/drawings/[id]/page.js
+++ src/app/drawings/[id]/page.js
@@ -6,14 +6,14 @@ import drawings from '@/Data/drawings'
 function drawingPage({params}) {
 
 const drawingRender = drawings.map(drawing => {if(drawing.id === params.id) 
-  {return <div key={drawing.id}>
+  {return <div className="drawings" key={drawing.id}>
     <h1>{drawing.title}</h1>
     <h2>{drawing.description}</h2>
     <h3>{drawing.image}</h3>
     <Image 
             src={drawing.image}
             width={600}
-            height={600}
+            height={750}
             alt="cartoon faces"
             />
     </div>}})
