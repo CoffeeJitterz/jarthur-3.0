0ce0fbee24cbaa5223c5914e22fc376e4151a780
CoffeeJitterz <josh.millard10@gmail.com>
2025-01-20 20:24:16 -0500

    small changes

--- src/app/album/page.js
+++ src/app/album/page.js
@@ -7,12 +7,13 @@ export default async function Album({searchParams}) {
     const pattern = params.pattern;
     return (
       <div className={`page_${pattern}`}>
-        <Card color={'rgb(255, 217, 2)'}>
+        <Card name={"text"} color={'rgb(255, 217, 2)'}>
           <h1>The Unfinished Album</h1>
           <p>The boys have been labouring for years in the studio, working diligently
             on a currently unfinished but completely realized double lp concept album. 
             The songs are writter. The tones are dialed in. But the time remains unpaid 
-            for. It might even be a triple lp.</p>
+            for. It might even be a triple lp. It's shrouded with mystery, but some mysteries
+            get solved. </p>
         </Card>        
       </div>
     );
