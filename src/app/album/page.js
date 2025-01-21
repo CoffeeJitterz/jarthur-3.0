e883e0f017d76e7c7c8de72ca076dd11b8f92c10
CoffeeJitterz <josh.millard10@gmail.com>
2025-01-20 20:17:13 -0500

    card and text added to the album page

--- src/app/album/page.js
+++ src/app/album/page.js
@@ -1,3 +1,4 @@
+import Card from "@/Components/Card";
 
 export default async function Album({searchParams}) {
 
@@ -6,7 +7,13 @@ export default async function Album({searchParams}) {
     const pattern = params.pattern;
     return (
       <div className={`page_${pattern}`}>
-        <h1>The Unfinished Album</h1>
+        <Card color={'rgb(255, 217, 2)'}>
+          <h1>The Unfinished Album</h1>
+          <p>The boys have been labouring for years in the studio, working diligently
+            on a currently unfinished but completely realized double lp concept album. 
+            The songs are writter. The tones are dialed in. But the time remains unpaid 
+            for. It might even be a triple lp.</p>
+        </Card>        
       </div>
     );
   }
